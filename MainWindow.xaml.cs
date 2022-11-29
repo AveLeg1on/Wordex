9a518d872db7f9e6e8831cacafa76ccbf621dbb3
ИСП-31 <-31@LAB17-11>
2022-11-29 13:29:08 +0300

    HatFix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -44,15 +44,17 @@ namespace Wordex
                     matches = regex.Matches(tmp);
                     if (matches.Count > 0)
                     {
-                        tmp = null;
-                        for (int z = 0; z < matches.Count; z++)
-                        {
-                            tmp += matches[z].ToString() + " ";
-                        }
+                        //tmp = null;
+                        //for (int z = 0; z < matches.Count; z++)
+                        //{
+                        //    tmp += matches[z].ToString() + " ";
+                        //}
+
 
                         i = LB.SelectedIndex;
                         LB.Items.RemoveAt(i);
-                        LB.Items.Insert(i, tmp);
+                        LB.Items.Insert(i, string.Join(" ",regex.Matches(tmp)));
+
                     }
                 }
                 else
@@ -93,7 +95,7 @@ namespace Wordex
         private void FAQ(object sender, RoutedEventArgs e)
         {
             
-            helps.Content = " Разработчик: Денисов Олег. 1) Дана строка '23 2+3 2++3 2+++3 445 677'. Напишите \nрегулярное выражение, которое найдет строки 23, 2+3, 2++3, 2+++3, не захватив остальные.\r\n2)Дана строка '*+ *q+ *qq+ *qqq+ *qqq qqq+'.\n Напишите регулярное выражение, \r\nкоторое найдет строки *q+, *qq+, *qqq+, не захватив остальные";
+            helps.Content = " Разработчик: Денисов Олег. 1) Дана строка '23 2+3 2++3 2+++3 445 677'. Напишите \nрегулярное выражение, которое найдет строки 23, 2+3, 2++3, 2+++3, не захватив остальные.\r\n2)Дана строка '*+ *q+ *qq+ *qqq+ *qqq qqq+'.+ Напишите регулярное выражение, \r\nкоторое найдет строки *q+, *qq+, *qqq+, не захватив остальные";
         }
     }
 }
